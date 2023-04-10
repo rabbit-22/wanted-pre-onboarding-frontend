51e2c0d7ef27837d0acd11917778b49f07872cb3
rabbit-22 <chlwlal20@gmail.com>
2023-04-11 01:19:03 +0900

    feat: handleCheck todo

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { TodoProps } from '../../types/todo';
+import { updateTodo } from '../../api/todo';
 
 /*
  * Todo 개별 요소
@@ -9,12 +10,24 @@ interface ItemProps {
   todo: TodoProps;
 }
 const TodoItem = ({ todo }: ItemProps) => {
+  const [isChecked, setIsChecked] = useState(todo.isCompleted);
+  const handleCheck = useCallback(() => {
+    updateTodo(todo.todo, !isChecked, todo.id);
+    setIsChecked((prev) => !prev);
+  }, [isChecked]);
+
   return (
     <li style={{ listStyle: 'none' }}>
       <label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={isChecked || false}
+          onChange={handleCheck}
+        />
         <span style={{ marginLeft: 3 }}>{todo.todo}</span>
       </label>
+      <button data-testid="modify-button">수정</button>
+      <button data-testid="delete-button">삭제</button>
     </li>
   );
 };
